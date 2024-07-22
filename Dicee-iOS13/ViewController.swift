87d25c444fcdfae0653d52ef1675b97ff947ee93
Fried-wogns20 <84609921+wogns-kim@users.noreply.github.com>
2024-07-22 23:54:34 +0900

    commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+            
+        diceImageView1.image
     }
 
 
